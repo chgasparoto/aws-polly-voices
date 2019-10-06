4a9f258329e460accfd86d21e2d9d44a3eb2533d
Yash <yashguptaofficial@gmail.com>
2019-10-06 15:04:28 +0530

    02 Add Indian English helper method

--- src/index.js
+++ src/index.js
@@ -81,6 +81,9 @@ export default class Voices {
   polish () {
     return this.byLangCode('pl-PL')
   }
+  indianEnglish () {
+    return this.byLangCode('en-IN')
+  }
 
   female () {
     return this.byGender('female')
